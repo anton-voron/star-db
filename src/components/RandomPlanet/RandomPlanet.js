067e54fa5083dcd9b3fdd301f8d1ed00ab035157
anton-voron <anton.voron.gt@gmail.com>
2019-06-12 18:18:48 +0300

    change of  Random Planet

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -1,41 +1,66 @@
 import React, { Component } from 'react';
 import SwapiService from '../../services/SwapiService .js';
-import Spinner from '../Spinner/Spinner.js'
+import Spinner from '../Spinner/Spinner.js';
+import ErrorIndicator from '../ErrorIndicator/ErrorIndicator.js';
 
 import './RandomPlanet.css';
 
 export default class RandomPlanet extends Component {
-
-    swapiService = new SwapiService();
+  swapiService = new SwapiService();
 
   state = {
     planet: {},
-    loading: true
+    loading: true,
+    error: false
   };
 
-constructor () {
+/*constructor () {
   super();
+  // Конструктор не должен делать запросы к серверу, это считается плохой практикой. 
+  //Его сложнее тестировать и работать. Поэтому делаем все запросы в componentDidMount 
+}*/
+
+componentDidMount () {
   this.updatePlanet();
+  this.interval = setInterval(this.updatePlanet, 5000); // создаём таймер id, по которому очистим ссылку на метод, когда он станет ненужным, ибо без этого он не удалитьься и будет постоянно делать запрос на сервер
+}
+
+componentWillUnmount() {
+  clearInterval(this.interval); //Нужно удалить данные, с которыми работал RandomPlanet , когда он закрывается, ибо эти элементы не удаляться и на фоне будет происходить запрос к серверу  дальше
+}
+
+onError = (err) => {
+  this.setState({
+    error: true, 
+    loading: false
+  });
 }
 
 onPlanetLoaded = (planet) => {
+  console.log('update');
   this.setState({
     planet,
     loading: false
   })
 };
-  updatePlanet() {
+  updatePlanet = () => {
     const id = Math.floor(Math.random()*25) + 2; // выбираем рандомную планету
     this.swapiService.getPlanet(id)
       .then(this.onPlanetLoaded)
+      .catch(this.onError) // перехват ошибки
   }
 
   render() {
-    const { planet, loading} = this.state;
+    const { planet, loading, error} = this.state;
+
+    const hasData = !(loading || error)
+
+    const errorIndicator = error ? <ErrorIndicator /> : null // обработка ошибки
     const spinner = loading ? <Spinner /> : null //Воспользуемся свойством JSX разметки (null) не отображается 
-    const content = !loading ? <PlanetView planet = {planet} /> : null
+    const content = hasData ? <PlanetView planet = {planet} /> : null
     return (
       <div className="random-planet jumbotron rounded">
+          {errorIndicator}
           {spinner}
           {content}
       </div>
@@ -45,15 +70,15 @@ onPlanetLoaded = (planet) => {
 }
 
 
-
+// Для того, чтобы не создавать лишние реакт компоненты, мы использует фрагмент реакта - это элемент обёртка для того, чтобы сгруппировать другие элементы
 const PlanetView = ({planet}) => {
   const { id, name, population, 
           rotationPeriod, diameter} = planet;
   return (
-      // Для того, чтобы не создавать лишние реакт компоненты, мы использует фрагмент реакта - это элемент обёртка для того, чтобы сгруппировать другие элементы
     <React.Fragment> 
       <img className="planet-image"
-            src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+            src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} 
+            alt="planet-img"/>
       <div>
         <h4>{name}</h4>
         <ul className="list-group list-group-flush">
