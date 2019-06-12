237873a1d70ad5ddcac5e741854be8af48b00d67
anton-voron <49334029+anton-voron@users.noreply.github.com>
2019-06-12 14:28:06 +0300

    component did update + did mount

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Header from '../Header/Header.js';
 import RandomPlanet from '../RandomPlanet/RandomPlanet.js';
 import ItemList from '../ItemList/ItemList.js';
 import PersonDetails from '../PersonDetails/PersonDetails.js';
 
+
 import './App.css';
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
+class App extends Component {
 
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
+  state = {
+    showRandomPlanet: true,
+    selectedPerson: 5
+  }
+
+  OnPersonSelected = (id) => {
+    this.setState({
+      selectedPerson: id
+    })
+  }
+  render() {
+    const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
+    return (
+      <div>
+        <Header />
+        {planet}
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList OnItemSelected = {this.OnPersonSelected}/>
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId = {this.state.selectedPerson}/>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } 
 };
 
 export default App;
